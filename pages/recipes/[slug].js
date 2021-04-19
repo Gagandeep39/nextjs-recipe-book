9e9dd3cd002503b1d58001e6f9b1771cf7262c2c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-19 18:48:52 +0000

    Added logic to generate path

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,25 @@
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: 'recipe' });
+
+  // Generate data for all pths
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+  return <div>Recipe Details</div>;
 }
