cc067d44a0a985b3426c0f5090e6f5123fdd1bbf
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-19 18:13:45 +0000

    Added basic display logic

---
+++ components/RecipeCard.js
@@ -0,0 +1,5 @@
+const RecipeCard = ({ recipe }) => {
+  return <div>{recipe.fields.title}</div>;
+};
+
+export default RecipeCard;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful';
+import RecipeCard from '../components/RecipeCard';
 
 export async function getStaticProps() {
   const client = createClient({
@@ -16,6 +17,11 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
-  console.log(recipes);
-  return <div className="recipe-list">Recipe List</div>;
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+    </div>
+  );
 }
