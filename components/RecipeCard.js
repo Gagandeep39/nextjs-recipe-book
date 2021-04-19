b677cec8fbd23bca663cd35f302776a3f42229c9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-19 18:21:34 +0000

    Created RecipeCard

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+
 const RecipeCard = ({ recipe }) => {
-  return <div>{recipe.fields.title}</div>;
+  const { title, slug, cookingTime, thumbnail } = recipe.fields;
+  return (
+    <div className="card">
+      <div className="featured">{/* Image */}</div>
+      <div className="content">
+        <div className="info">
+          <h4> {title} </h4>
+          <p> Takes approx {cookingTime} mins to make </p>
+        </div>
+        <div className="actions">
+          <Link href={`/recipes/${slug}`}>
+            <a>Cook this</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeCard;
