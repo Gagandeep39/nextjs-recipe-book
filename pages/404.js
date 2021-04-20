8398e72bfa6930ccdb9f8d7025ef42a321cd0bd1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-20 20:01:26 +0000

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Ooops! The Page cannot be found </h2>
+      <p>
+        Redirecting to <Link href="/">Homepage</Link> for more recipes
+      </p>
+
+      <style jsx>{`
+        .not-found {
+          background: #fff;
+          padding: 30px;
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          transform: rotateZ(-1deg);
+        }
+        h1 {
+          font-size: 3em;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default NotFound;
