9690089a1e8dae8b7e157e889cf95f1be2951980
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-20 12:51:52 +0000

    Added markdown text

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,5 +1,6 @@
 import { createClient } from 'contentful';
 import Image from 'next/image';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -52,7 +53,7 @@ export default function RecipeDetails({ recipe }) {
           width={featuredImage.fields.file.details.image.width}
           height={featuredImage.fields.file.details.image.height}
         />
-        <h2> {title} </h2>
+        <h2>{title}</h2>
       </div>
       <div className="info">
         <p>Take about {cookingTime} mins to cook. </p>
@@ -64,8 +65,34 @@ export default function RecipeDetails({ recipe }) {
 
       <div className="method">
         <h3>Method: </h3>
-        <div></div>
+        <div> {documentToReactComponents(method)} </div>
       </div>
+      <style jsx>{`
+        h2,
+        h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ', ';
+        }
+        .info span:last-child::after {
+          content: '.';
+        }
+      `}</style>
     </div>
   );
 }
